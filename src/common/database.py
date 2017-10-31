242175f72e80659ea313fe803740ffe850590d04
Brian Dowe <briandowe@gmail.com>
2017-10-30 18:03:09 -0700

    Add heroku mongo url to database.py

---
+++ src/common/database.py
@@ -0,0 +1,31 @@
+import os
+import pymongo
+
+class Database(object):
+    URI = os.environ.get("MONGOLAB_URI")
+    DATABASE = None
+
+    @staticmethod
+    def initialize():
+        client = pymongo.MongoClient(Database.URI)
+        Database.DATABASE = client["fullstack_python"]
+
+    @staticmethod
+    def insert(collection, data):
+        Database.DATABASE[collection].insert(data)
+
+    @staticmethod
+    def find(collection, query):
+        return Database.DATABASE[collection].find(query)
+
+    @staticmethod
+    def find_one(collection, query):
+        return Database.DATABASE[collection].find_one(query)
+
+    @staticmethod
+    def update(collection, query, data):
+        Database.DATABASE[collection].update(query, data, upsert=True)
+
+    @staticmethod
+    def remove(collection, query):
+        Database.DATABASE[collection].remove(query)
